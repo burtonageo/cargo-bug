a9be2308f5e85cf160a23c33da76ed3992d7b526
George Burton <burtonageo@gmail.com>
2015-07-24 19:20:57 +0100

    Fix movement, and refactor

--- src/game.rs
+++ src/game.rs
@@ -2,16 +2,16 @@ use glutin_window::GlutinWindow;
 use na;
 use na::{Translate, Pnt2, Vec2};
 use opengl_graphics::{GlGraphics, OpenGL};
-use piston::input::{Input, RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
+use piston::input::{Event, Input, RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::event_loop::{EventMap, Events};
-use simplecolor::Rgba;
+use sc::Rgba;
 use std::cell::RefCell;
 use std::rc::Rc;
 
 pub struct Game {
     gl: GlGraphics,
     bg_color: Rgba<f32>,
-    entity: Entity,
+    hero: Hero,
     window: Rc<RefCell<GlutinWindow>>
 }
 
@@ -20,7 +20,7 @@ impl Game {
         Game {
             gl: GlGraphics::new(opengl),
             bg_color: Rgba::with_components(0.0, 1.0, 0.0, 1.0),
-            entity: Entity::new(
+            hero: Hero::new(
                 Rgba::with_components(1.0, 0.0, 0.0, 1.0),
                 Pnt2::new(0.0, 0.0),
                 50.0),
@@ -30,59 +30,67 @@ impl Game {
 
     pub fn run_loop(&mut self) {
         for e in self.window.clone().events() {
-            use piston::input::Event;
-
-            if let Event::Render(r) = e {
-                self.render(&r);
-            }
-
-            if let Event::Update(u) = e {
-                self.update(&u);
-            }
-
-            if let Event::Input(i) = e {
-                self.input(&i);
+            match e {
+                Event::Render(r) => {
+                    self.render(&r);
+                },
+                Event::Update(u) => {
+                    self.update(&u);
+                },
+                Event::Input(i) => {
+                    self.input(&i);
+                },
+                _ => { }
             }
         }
     }
 
-    pub fn render(&mut self, args: &RenderArgs) {
+    fn render(&mut self, args: &RenderArgs) {
         use graphics::clear;
 
         let bg_col = self.bg_color.to_slice();
         self.gl.draw(args.viewport(), |_, gl| { clear(bg_col, gl); });
-        self.entity.render(&mut self.gl, args);
+        self.hero.render(&mut self.gl, args);
     }
 
-    pub fn update(&mut self, args: &UpdateArgs) {
-        self.entity.update(args);
+    fn update(&mut self, args: &UpdateArgs) {
+        self.hero.update(args);
     }
 
-    pub fn input(&mut self, args: &Input) {
-        self.entity.input(&args)
+    fn input(&mut self, args: &Input) {
+        self.hero.input(&args)
     }
-
 }
 
-struct Entity {
+pub trait Entity {
+    fn input(&mut self, _: &Input) { }
+    fn update(&mut self, _: &UpdateArgs) { }
+    fn render(&mut self, _: &mut GlGraphics, _: &RenderArgs) { }
+}
+
+pub struct Hero {
     color: Rgba<f32>,
     topleft: Pnt2<f64>,
-    velocity: Vec2<f64>,
+    curr_velocity: Vec2<f64>,
+    max_velocity: Vec2<f64>,
     size: f64,
     rotation: f64
 }
 
-impl Entity {
-    fn new(col: Rgba<f32>, tl: Pnt2<f64>, sz: f64) -> Entity {
-        Entity {
+impl Hero {
+    fn new(col: Rgba<f32>, tl: Pnt2<f64>, sz: f64) -> Hero {
+        Hero {
             color: col,
             topleft: tl,
-            velocity: na::zero(),
+            curr_velocity: na::zero(),
+            max_velocity: Vec2::new(5.0, 5.0),
             size: sz,
             rotation: 0.0
         }
     }
+}
 
+impl Entity for Hero {
     fn render(&mut self, gl: &mut GlGraphics, args: &RenderArgs) {
         use graphics::*;
         let square = rectangle::square(self.topleft[0], self.topleft[1], self.size);
@@ -98,9 +106,15 @@ impl Entity {
     }
 
     fn update(&mut self, args: &UpdateArgs) {
-        self.velocity = self.velocity * args.dt;
-        // self.rotation += 2.0 * args.dt;
-        self.topleft = self.velocity.translate(&self.topleft);
+        use std::ops::Neg;
+        fn clamp_velocity<F: PartialOrd + na::BaseFloat + Neg>(vel: &Vec2<F>, max: &Vec2<F>) -> Vec2<F> {
+            use na::clamp;
+            Vec2::new(clamp(vel[0], max[0].neg(), max[0]),
+                      clamp(vel[1], max[1].neg(), max[1]))
+        }
+
+        self.topleft = clamp_velocity(&(self.curr_velocity * args.dt), &self.max_velocity)
+            .translate(&self.topleft);
     }
 
     fn input(&mut self, iput: &Input) {
@@ -109,24 +123,24 @@ impl Entity {
 
         match iput {
             &Input::Press(Button::Keyboard(key)) => {
-                const MAX_VELOCITY: f64 = 500.0;
+                const VELOCITY_INCREMENT: f64 = 500.0;
                 if let Key::Up = key {
-                    self.velocity[1] -= MAX_VELOCITY;
+                    self.curr_velocity[1] -= VELOCITY_INCREMENT;
                 }
 
                 if let Key::Down = key {
-                    self.velocity[1] += MAX_VELOCITY;
+                    self.curr_velocity[1] += VELOCITY_INCREMENT;
                 }
 
                 if let Key::Left = key {
-                    self.velocity[0] -= MAX_VELOCITY;
+                    self.curr_velocity[0] -= VELOCITY_INCREMENT;
                 }
 
                 if let Key::Right = key {
-                    self.velocity[0] += MAX_VELOCITY;
+                    self.curr_velocity[0] += VELOCITY_INCREMENT;
                 }
             },
-            &Input::Release(Button::Keyboard(_)) => { self.velocity = na::zero(); },
+            &Input::Release(Button::Keyboard(_)) => { self.curr_velocity = na::zero(); },
             _ => { }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
-#![feature(const_fn)]
+//#![feature(const_fn)]
 
 extern crate piston;
 extern crate graphics;
 extern crate glutin_window;
 extern crate opengl_graphics;
-extern crate simplecolor;
+extern crate simplecolor as sc;
 extern crate nalgebra as na;
 
 mod game;
 
 use piston::window::WindowSettings;
-use glutin_window::GlutinWindow;
 use opengl_graphics::OpenGL;
 
 use game::Game;
@@ -18,10 +17,11 @@ use game::Game;
 fn main() {
     const OPENGL: OpenGL = OpenGL::V3_2;
 
-    let window = GlutinWindow::from(WindowSettings::new("calaxite", (800, 600))
-                                        .opengl(OPENGL)
-                                        .fullscreen(false)
-                                        .exit_on_esc(true));
+    let window = WindowSettings::new("calaxite", (800, 600))
+                     .opengl(OPENGL)
+                     .fullscreen(false)
+                     .exit_on_esc(true)
+                     .into();
 
     let mut game = Game::new(OPENGL, window);
 
